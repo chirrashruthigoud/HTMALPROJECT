0f6654941ec6100a63b637ad7fe365cefd7a8305
chirrashruthigoud <chirrashruthigoud4@gmail.com>
2023-05-17 15:52:41 +0530

    welcome to html

---
+++ ArrayListDemo.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HtmlProjectProgram
+{
+    internal class ArrayListDemo
+    {
+        public void arraylistdemo()
+        {
+            ArrayList arraylist = new ArrayList();
+            arraylist.Add(1);
+            arraylist.Add(2);
+            arraylist.Add("shruthi");
+            arraylist.Add(3);
+            arraylist.Add(5);
+            arraylist.Add(6);
+            //insert
+            // arraylist.Insert(0,9);
+            //arraylist.Insert(1,67);
+            //remove
+            //arraylist.Remove(6);
+            //arraylist.RemoveAt(4); //it will remove the data accoding to index
+            //--removerange
+            arraylist.RemoveRange(0, 3);//it will remove the first 3 count
+            int count = arraylist.Count;
+            Console.WriteLine("count" + count);
+
+            //arraylist.Clear();
+            //int counts = arraylist.Count;
+            //Console.WriteLine("count" + count);
+
+            arraylist.Sort();
+            //Console.WriteLine(arraylist);
+            foreach (var i in arraylist)
+            {
+                Console.WriteLine(i);
+            }
+
+            //for loop
+            Console.WriteLine("----for loop----");
+            for (int i = 0; i < arraylist.Count; i++) //we can use only count(index)
+            {
+                // Console.WriteLine(i);//by using this we can only index.
+                Console.WriteLine(arraylist[i]);
+            }
+        }
+    }
+}
---
+++ DictionaryProblem.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HtmlProjectProgram
+{
+   //dictionary : dictionary is generic collection that consist of element.
+    //as key value pair.
+    //it is not sorted order.
+    public class DictionaryProblem
+    {
+        Dictionary<String, String> dict = new Dictionary<string, string>();
+        public void DictionaryMethod()
+        {
+            dict.Add("1", "sunny");
+            dict.Add("2", "hara");
+            dict.Add("3", "hari");
+            dict.Add("4", "siri");
+            Console.WriteLine(dict["3"]);
+            Console.WriteLine(dict["4"]);
+            //update
+            dict["2"] = "sara";
+            //remove
+            dict.Remove("3");
+            foreach (KeyValuePair<String, String> key in dict)
+            {
+                Console.WriteLine("{0} : {1}", key.Key, key.Value);
+            }
+
+
+
+            Console.WriteLine("------");
+            Dictionary<int, String> dic = new Dictionary<int, String>();
+            dic.Add(1, "sweety");
+            dic.Add(2, "sony");
+            dic[3] = "sunny";
+            foreach (KeyValuePair<int, String> kala in dic)
+            {
+                Console.WriteLine("{0} : {1}", kala.Key, kala.Value);
+            }
+            if (dict.ContainsKey("2"))
+            {
+                dict["2"] = "shiva";
+            }
+            foreach (KeyValuePair<int, String> kala in dic)
+            {
+                Console.WriteLine("{0} : {1}", kala.Key, kala.Value);
+            }
+            dict.Clear();
+            // DELETE: remove an element from the dictionary
+            // CREATE: add new elements to the dictionary
+            foreach (KeyValuePair<int, String> kala in dic)
+            {
+                Console.WriteLine("{0} : {1}", kala.Key, kala.Value);
+            }
+        }
+    }
+}
