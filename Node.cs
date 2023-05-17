f450f0b0972623135acc7db2c034a73eff9ba91b
chirrashruthigoud <chirrashruthigoud4@gmail.com>
2023-05-17 16:02:36 +0530

    welcome to html

---
+++ Node.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HtmlProjectProgram
+{
+    public class Node
+    {
+        public int data;
+        public Node next;//Address of a node
+        //constructor
+        public Node(int data)
+        {
+            this.data = data;
+        }
+    }
+}
